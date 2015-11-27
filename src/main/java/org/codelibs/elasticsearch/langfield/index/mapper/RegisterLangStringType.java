499312a663e78281cb14dc02a385feb27ee512cc
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-11-27 22:33:58 +0900

    es 2.1 support

--- src/main/java/org/codelibs/elasticsearch/langfield/index/mapper/RegisterLangStringType.java
+++ src/main/java/org/codelibs/elasticsearch/langfield/index/mapper/RegisterLangStringType.java
@@ -1,19 +1,18 @@
 package org.codelibs.elasticsearch.langfield.index.mapper;
 
 import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.AbstractIndexComponent;
 import org.elasticsearch.index.Index;
 import org.elasticsearch.index.mapper.MapperService;
-import org.elasticsearch.index.settings.IndexSettings;
+import org.elasticsearch.index.settings.IndexSettingsService;
 
 public class RegisterLangStringType extends AbstractIndexComponent {
 
     @Inject
     public RegisterLangStringType(final Index index,
-            @IndexSettings final Settings indexSettings,
+            final IndexSettingsService indexSettingsService,
             final MapperService mapperService) {
-        super(index, indexSettings);
+        super(index, indexSettingsService.getSettings());
 
         mapperService.documentMapperParser().putTypeParser("langstring",
                 new LangStringFieldMapper.TypeParser());
